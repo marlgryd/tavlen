f2e11f14e2d905aa447a40251708dd458906af5e
Martin Rydving <marlgryd@gmail.com>
2022-07-04 15:56:17 +0200

    Update bilde3.py

--- bilde3.py
+++ bilde3.py
@@ -5,6 +5,7 @@ draw = ImageDraw.Draw(im)
 
 draw.ellipse((100, 100, 150, 200), fill=(0), outline=(1))
 draw.rectangle((200, 100, 300, 200), fill=(0), outline=(1))
-draw.line((350, 200, 450, 100), fill=(0), width=10)
+draw.line((350, 200, 450, 100), fill=(0), width=(10))
+draw.line((400, 240, 0, 0), fill=(0), width=(5))
 
 im.save('pillow_imagedraw3.bmp', quality=95)
