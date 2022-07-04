76688a7dd25aec3b1122a401b6dfda8266d7ed37
sigrube <108678650+sigrube@users.noreply.github.com>
2022-07-04 16:48:40 +0200

    rf

---
+++ bilde3s.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw
+
+im = Image.new('1', (800, 480), (1))
+draw = ImageDraw.Draw(im)
+
+draw.ellipse((100, 100, 150, 200), fill=(0), outline=(1))
+draw.rectangle((200, 100, 300, 200), fill=(0), outline=(1))
+draw.line((350, 200, 450, 100), fill=(0), width=(10))
+draw.line((400, 240, 0, 0), fill=(0), width=(5))
+
+im.save('pillow_imagedraw3.bmp', quality=95)
+
+
